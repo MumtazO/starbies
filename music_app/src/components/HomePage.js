51ee49a22b7a33361893b2697e39103db8602fa5
BushraFatimaBF <102679041+BushraFatimaBF@users.noreply.github.com>
2022-10-06 16:45:34 +0100

    created album display component

--- music_app/src/components/HomePage.js
+++ music_app/src/components/HomePage.js
@@ -1,9 +1,18 @@
-import "./HomePage.css"
+import "./homepage.css";
+import { Grid, GridItem } from '@chakra-ui/react';
+
+
+
 
 function HomePage () {
     return ( 
-        <div classname="album">
-            <p></p>
+        <div className="album">
+            <Grid templateColumns='repeat(3, 1fr)' 
+            gap={10} marginEnd={25} marginStart={20}>
+            <GridItem w='300px' h='200' bg='blue.500'/>
+            <GridItem w='300px' h='200' bg='blue.500' />
+            <GridItem w='300px' h='200' bg='blue.500' />
+            </Grid>
         </div>
      );
 }
--- music_app/src/pages/Layout.jsx
+++ music_app/src/pages/Layout.jsx
@@ -1,11 +1,13 @@
 import {Outlet} from "react-router-dom";
 import Footer from "../components/Footer.js"
-import Header from "../components/header.js";
+import Header from "../components/Header.js"
+
 
 const Layout = () =>{
     return (
         <>
         <Header />
+       
         <Footer/>
         <Outlet/>
         </>
