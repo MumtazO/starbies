e3acf208f9f902649256276d5a80ac264f7d0739
BushraFatimaBF <102679041+BushraFatimaBF@users.noreply.github.com>
2022-10-07 15:09:43 +0100

    fixed css for about and privacy policy page

---
+++ music_app/src/components/Albumpage.js
@@ -0,0 +1,8 @@
+function Albumpage(){
+    return(<>
+        <h1>This is the album page</h1>
+        <p>Here we will display a list of the songs from the selected album</p>
+    </>)
+}
+
+export default Albumpage
